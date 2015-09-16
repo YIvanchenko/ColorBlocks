a71c1684d76275ac2003117c2fb616b59cbc1158
Yuri Ivanchenko <yuri.ivanchenko.ua@gmail.com>
2015-09-16 18:39:15 +0200

    A solution stub with a unittests project was added.

---
+++ ColorBlocksGame/ColorBlocksCore/Model/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorBlocksCore.Model
+{
+    public class Board
+    {
+        private readonly Tile[,] _tiles;
+
+        public Board(int n, int m)
+        {
+            _tiles = new Tile[n, n];
+
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    _tiles[i, j] = new Tile(random.Next(m));
+                }
+            }
+        }
+    }
+}
---
+++ ColorBlocksGame/ColorBlocksCore/Model/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorBlocksCore.Model
+{
+    public class Tile
+    {
+        public Tile(int color)
+        {
+            Color = color;
+        }
+
+        public int Color { get; private set; }
+    }
+}
